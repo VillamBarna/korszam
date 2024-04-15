fd6fda9c7a7e0d738b9e8e1b6428ab2d560b5d5c
villambarna <barna.villam@gmail.com>
2024-04-15 15:41:40 +0200

    összeadás v2

--- beadando2/polinom.cpp
+++ beadando2/polinom.cpp
@@ -3,12 +3,9 @@
 #include "polinom.h"
 
 int main() {
-    std::vector<int> e{1,2,3};
-    polinom p{e};
-    std::vector<int> a{3,4,5};
-    polinom p2{a};
-    polinom p3{e};
-    p3 = p + p2;
+    polinom p{std::vector<int> {1,2,3}};
+    polinom p2{std::vector<int> {4,5,6}};
+    polinom p3 = p - p2;
     std::cout << "done" << std::endl;
     std::cout << p3.egyutthatok[0] << std::endl;
     std::cout << p3.egyutthatok[1] << std::endl;
--- beadando2/polinom.h
+++ beadando2/polinom.h
@@ -8,8 +8,14 @@ struct polinom {
     polinom (std::vector<T> e) : egyutthatok{e} {
     };
 
+    polinom () : egyutthatok{std::vector<T> {}} {
+    };
+
+    polinom (size_t hossz) : egyutthatok{std::vector<T> (hossz)} {
+    };
+
     polinom operator+(polinom<T> const& p2) const {
-        polinom<T> eredmeny{this->egyutthatok};
+        polinom<T> eredmeny{this->egyutthatok.size()};
 
         std::transform(this->egyutthatok.begin(),
                         this->egyutthatok.end(),
@@ -18,4 +24,15 @@ struct polinom {
                         [](T const& x, T const& y) { return x+y; });
         return eredmeny;
     }
+
+    polinom operator-(polinom<T> const& p2) const {
+        polinom<T> eredmeny{this->egyutthatok.size()};
+
+        std::transform(this->egyutthatok.begin(),
+                        this->egyutthatok.end(),
+                        p2.egyutthatok.begin(),
+                        eredmeny.egyutthatok.begin(),
+                        [](T const& x, T const& y) { return x-y; });
+        return eredmeny;
+    };
 };
