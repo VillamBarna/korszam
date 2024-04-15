3a841753bd307c29133db90f6471931bc204b45b
villambarna <barna.villam@gmail.com>
2024-04-15 14:27:47 +0200

    struct létrehozása, cmake config

--- beadando2/polinom.cpp
+++ beadando2/polinom.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <vector>
+#include "polinom.h"
 
+
+int main() {
+    polinom p;
+    p.egyutthatok = {1, 2, 3};
+    std::cout << p.egyutthatok[0] << std::endl;
+}
---
+++ beadando2/polinom.h
@@ -0,0 +1,5 @@
+#include <vector>
+
+struct polinom {
+    std::vector<double> egyutthatok;
+};
