4a106faec29e7fd51a71ada48d13a583f3888a51
villambarna <barna.villam@gmail.com>
2024-04-15 15:25:45 +0200

    összeadás v1

--- beadando2/polinom.cpp
+++ beadando2/polinom.cpp
@@ -2,9 +2,15 @@
 #include <vector>
 #include "polinom.h"
 
-
 int main() {
-    polinom p;
-    p.egyutthatok = {1, 2, 3};
-    std::cout << p.egyutthatok[0] << std::endl;
+    std::vector<int> e{1,2,3};
+    polinom p{e};
+    std::vector<int> a{3,4,5};
+    polinom p2{a};
+    polinom p3{e};
+    p3 = p + p2;
+    std::cout << "done" << std::endl;
+    std::cout << p3.egyutthatok[0] << std::endl;
+    std::cout << p3.egyutthatok[1] << std::endl;
+    std::cout << p3.egyutthatok[2] << std::endl;
 }
--- beadando2/polinom.h
+++ beadando2/polinom.h
@@ -1,5 +1,21 @@
 #include <vector>
+#include <algorithm>
 
+template<typename T>
 struct polinom {
-    std::vector<double> egyutthatok;
+    std::vector<T> egyutthatok;
+
+    polinom (std::vector<T> e) : egyutthatok{e} {
+    };
+
+    polinom operator+(polinom<T> const& p2) const {
+        polinom<T> eredmeny{this->egyutthatok};
+
+        std::transform(this->egyutthatok.begin(),
+                        this->egyutthatok.end(),
+                        p2.egyutthatok.begin(),
+                        eredmeny.egyutthatok.begin(),
+                        [](T const& x, T const& y) { return x+y; });
+        return eredmeny;
+    }
 };
