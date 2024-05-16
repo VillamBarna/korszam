fb7234362d4ab5c037d0ca5189b48f063872e8a1
villambarna <barna.villam@gmail.com>
2024-05-16 18:43:51 +0200

    javitas

--- beadando3/main.py
+++ beadando3/main.py
@@ -7,7 +7,6 @@ x2 = tf.random.uniform([32, 10, 100, 3], dtype=tf.float32)
 # dimenió szerint az összes váriáció
 x1 = tf.expand_dims(x1, axis=-2)
 x2 = tf.expand_dims(x2, axis=-3)
-print((x1-x2)[0, 0, 0, 0])
 
 # Kiszámítjuk a különbség vektor hosszát, az utolsó dimenzióban
 x3 = tf.norm(x1-x2, ord='euclidean', axis=-1)
